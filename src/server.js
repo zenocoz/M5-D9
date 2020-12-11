3578fe34cc88428f71d9f3bc09dcbacaba945e05
zenocoz <federico.soncini@gmail.com>
2020-12-11 10:36:59 +0100

    server.js boilerplate

--- src/server.js
+++ src/server.js
@@ -1,9 +1,28 @@
-//TOOLS AND MIDDLEWARE
+//---------------------Require
 const express = require("express")
+const cors = require("cors")
 
-//Instances
+const {
+  notFoundErrorHandler,
+  unauthorizedErrorHandler,
+  forbiddenErrorHandler,
+  badRequestErrorHandler,
+  catchAllErrorHandler,
+} = require("./errorHandling")
+
+//---------------------Instances
 const server = express()
 
-const port = process.env.PORT || 3001
+//-----------------------Use
+server.use(cors())
+server.use(express.json())
+//errors
+server.use(notFoundErrorHandler)
+server.use(unauthorizedErrorHandler)
+server.use(forbiddenErrorHandler)
+server.use(badRequestErrorHandler)
+server.use(catchAllErrorHandler)
 
+//---------------------Listen
+const port = process.env.PORT || 3001
 server.listen(port, () => console.log("server created on port", port))
